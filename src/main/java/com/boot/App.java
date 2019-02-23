20dd03c9a11fa59fe31bd029319b32a33bec4e04
JoePullen <joe.pullen@espalier.com>
2019-02-23 15:46:09 +0100

    test

--- src/main/java/com/boot/App.java
+++ src/main/java/com/boot/App.java
@@ -3,6 +3,9 @@ package com.boot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+ * Testing
+ */
 @SpringBootApplication
 public class App {
     public static void main( String[] args ) {
